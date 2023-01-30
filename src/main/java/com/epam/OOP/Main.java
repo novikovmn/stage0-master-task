3df791eb4e464672d39d3c957257f949d6457139
novikovmn <genrigale@gmail.com>
2023-01-30 22:59:02 +0300

    done. added Main.java

---
+++ src/main/java/com/epam/OOP/Main.java
@@ -0,0 +1,16 @@
+package com.epam.OOP;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Bird bird = new Bird();
+		Dog dog = new Dog();
+		
+		System.out.println(bird.getDescription());
+		
+		System.out.println(dog.getDescription());
+
+	}
+
+}
